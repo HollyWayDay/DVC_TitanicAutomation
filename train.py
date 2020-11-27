67f8a51619592eb00a4d030883b1c0563894ce05
Nakshatra Singh <53419293+nakshatrasinghh@users.noreply.github.com>
2020-11-27 13:47:39 +0530

    try RFC

--- train.py
+++ train.py
@@ -46,7 +46,7 @@ X_test = scaler_x.transform(X_test)
 accuracies = [] 
 
 #@ Initializing the Logistic Regression Algorithm 
-classifier = RandomForestClassifier()
+classifier = RandomForestClassifier(n_estimators=1000, criterion='gini')
 
 #@ Fitting the classifier 
 classifier.fit(X_train, y_train) 
